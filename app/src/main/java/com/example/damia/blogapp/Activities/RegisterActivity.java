a51a667a1f06363d4f43d27930a8a6b571c31531
damiaNR10 <dymon10@o2.pl>
2018-09-10 10:01:22 +0200

    cmt

--- app/src/main/java/com/example/damia/blogapp/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/damia/blogapp/Activities/RegisterActivity.java
@@ -1,13 +1,18 @@
 package com.example.damia.blogapp.Activities;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.damia.blogapp.Manifest;
 import com.example.damia.blogapp.R;
@@ -15,6 +20,9 @@ import com.example.damia.blogapp.R;
 public class RegisterActivity extends AppCompatActivity {
 
     ImageView imgUserPhoto;
+    static int PReqCode = 1;
+    static int REQUESTCODE = 1;
+    Uri pickedImgUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +45,38 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void openGallery() {
 
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQUESTCODE);
+
     }
 
     private void checkAndRequestForPermission() {
 
-        //if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE != PackageManager.PERMISSION_GRANTED)){
+        if(ContextCompat.checkSelfPermission(RegisterActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE)){
 
-        //}
+                Toast.makeText(RegisterActivity.this,"Please accept for required permission", Toast.LENGTH_SHORT).show();
 
+            }
+            else {
+                ActivityCompat.requestPermissions(RegisterActivity.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, PReqCode);
+            }
+        }
+        else{
+            openGallery();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(resultCode == RESULT_OK && requestCode == REQUESTCODE && data != null){
+
+            pickedImgUri = data.getData();
+            imgUserPhoto.setImageURI(pickedImgUri);
+
+        }
     }
 }
